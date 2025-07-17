1b5267ab71c18147cb7effcfd81e792201332283
halfy1 <54471372+halfy1@users.noreply.github.com>
2025-07-17 11:16:42 +0300

    Mem usage

--- server-stash.sh
+++ server-stash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [-z "$BASH_VERSION"]; then 
+if [ -z "$BASH_VERSION"]; then 
     echo "Пожалуйста, запустите скрпит с помощью bash"
     exit 1
 fi
@@ -11,9 +11,9 @@ echo
 echo "CPU USAGE"
 
 CPU_LINE=$(top -bn1 | grep "^%Cpu" || top -bn1 | grep "Cpu(s)")
-#CPU_LINE=$(echo "$CPU_LINE" | awk -F',' '{for(i=1; i<=NF;i++) if ($1 ~ /id/) print $1}' | grep -o '[0-9]*')
+#CPU_LINE=$(echo "$CPU_LINE" | awk -F',' '{for(i=1; i<=NF;i++) if ($i ~ /id/) print $1}' | grep -o '[0-9]*')
 
-IDLE_FIELD=$(echo "CPU_LINE" | awk -F', ' '
+IDLE_FIELD=$(echo "$CPU_LINE" | awk -F', ' '
     {
         for(i=1; i<NF; i++) {
             if ($1 ~ /id/){
@@ -25,5 +25,20 @@ IDLE_FIELD=$(echo "CPU_LINE" | awk -F', ' '
 
 CPU_IDLE=$(echo "$IDLE_FIELD" | grep -o '[0-9]*')
 CPU_USED=$(awk "BEGIN {print 100 - $CPU_IDLE}")
+
+echo "TOTAL CPU Usage: $CPU_USED%"
+
+echo "Memory Usage:"
+
+read -r MEM_TOTAL MEM_USED MEM_FREE <<< $(free -m | awk '/^Mem:/ {print $2, $3, $4}')
+
+MEM_USED_PERCENT=$(awk "BEGIN {printf \"%.1f", ($MEM_USED/$MEM_TOTAL)*100}")
+
+echo "Total Memory: ${MEM_TOTAL} MB"
+echo "Used Memory:  ${MEM_USED} MB"
+echo "Free Memory:  ${MEM_FREE} MB"
+echo "Memory Usage: ${MEM_USED_PERCENT}%"
+
+echo
 
 echo "TOTAL CPU Usage: $CPU_USED%"
